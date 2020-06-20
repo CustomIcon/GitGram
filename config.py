8a57038b6f377d34c5d4227ff765209e5a244162
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2020-06-20 11:23:21 +0000

    [config] Update the config file
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ BOT_TOKEN = ""
 
 # Project Name of your Project
 PROJECT_NAME = ""
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
