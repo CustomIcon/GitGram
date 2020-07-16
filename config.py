28d105d31d5aa425192de841b2c1f8e824d801df
pokurt <amanhmedzahir@hotmail.com>
2020-07-17 04:47:01 +0500

    added gitignore for config file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = ""
+BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
 
 # Project Name of your Project
-PROJECT_NAME = ""
+PROJECT_NAME = "GitGram"
 
 # If you forked the source code, grab your repo's link and paste it below!
 GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
